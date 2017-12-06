7e5541183a6575da6430e46164c32871d139ec5e
bill perkins <wperkins@hearst.com>
2017-12-06 11:26:33 -0500

    additions

--- std.php
+++ std.php
@@ -4,9 +4,21 @@
 
 error_reporting(E_ALL);
 date_default_timezone_set('America/New_York');
-$log = fopen("program.log", "a"); # use "w" if we want a fresh log file
+
+define("EXEC_PATH", dirname(__FILE__)); # program resides at EXEC_PATH (const)
+
+# $argv and $argc:
+
+$pgmname = $argv[0]; # this program's name
+$logname = str_ireplace(".php", ".log", $pgmname);
+
+if ($argc > 1) {     # means we have some args passed to us
+    $arg1 = argv[1]; #
+    # etc...
+}
 
 # do this when logging:
+$log = fopen($logname, "a"); # use "w" if we want a fresh log file
 fprintf($log, date('Y-m-d H:i:s ') . "\n");
 
 # EOF:
