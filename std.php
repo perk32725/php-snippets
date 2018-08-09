2626cfc5bb378ac2daa652f75589f47378ea3f2a
bill perkins <wperkins@hearst.com>
2018-08-09 12:40:52 -0400

    add checks for pcntl_* functions

--- std.php
+++ std.php
@@ -6,9 +6,10 @@ error_reporting(E_ALL);
 date_default_timezone_set('America/New_York');
 define("EXEC_PATH", dirname(__FILE__)); # program resides at EXEC_PATH (const)
 
-$iAm     = basename(__FILE__);
-$myPid   = getmypid();
-$logfile = null; # can be set to STDOUT or STDERR
+$iAm      = basename(__FILE__);
+$myPid    = getmypid();
+$logfile  = null; # can be set to null, STDOUT, or STDERR
+$childPid = 0;
 
 # --- set logname to something reasonable:
 $logname = "/var/log/" . str_replace(".php", ".log", $iAm);
@@ -27,7 +28,10 @@ function stdlog($msg) {
     global $myPid;
 
     if (!$logfile) {
-        $logfile = fopen($logname, "a");
+        if (!($logfile = fopen($logname, "a"))) {
+            fprintf(STDERR, date('Y-m-d H:i:s ') . "[$myPid] couldn't fopen() $logname, exiting\n");
+            exit;
+        }
     }
 
     fprintf($logfile, date('Y-m-d H:i:s ') . "[$myPid] $msg\n");
@@ -36,20 +40,24 @@ function stdlog($msg) {
 #-----------------------------------
 # on SIGTERM, exit
 #-----------------------------------
-function term_routine() {
-    global $childPid;
+if (function_exists('pcntl_signal')) {
+    function term_routine() {
+        global $childPid;
 
-    $status = 0;
+        $status = 0;
 
-    if ($childPid) {
-        stdlog("posix_kill($childPid, SIGKILL)");
-        posix_kill($childPid, SIGKILL);
-        stdlog("waiting for child [$childPid]");
-        pcntl_wait($status); # Protect against Zombie children
-        stdlog("child process [$childPid] returned $status");
+        if ($childPid) {
+            stdlog("posix_kill($childPid, SIGKILL)");
+            posix_kill($childPid, SIGKILL);
+            stdlog("waiting for child [$childPid]");
+            pcntl_wait($status); # Protect against Zombie children
+            stdlog("child process [$childPid] returned $status");
+        }
+
+        exit;
     }
 
-    exit;
+    pcntl_signal(SIGTERM, 'term_routine');
 }
 
 #------------------------------------
@@ -59,32 +67,33 @@ function exit_routine() {
     stdlog("(exit_routine())");
 }
 
-# --- register TERM signal handler and exit_routine:
-pcntl_signal(SIGTERM, 'term_routine');
+# --- register exit_routine to be run on shutdown:
 register_shutdown_function('exit_routine');
 
 # --- do a fork and exec:
-$childPid = pcntl_fork();
-if ($childPid == -1) {
-    die('could not fork');
-} else if ($childPid == 0) { #  we are the child
-    # build command, command array:
-    $cmd = "COMMAND";
-    $cmdarray[] = "OPTION1";
-    $cmdarray[] = "OPTION2";
-    # etc...
+if (function_exists('pcntl_fork')) {
+    $childPid = pcntl_fork();
+    if ($childPid == -1) {
+        die('could not fork');
+    } else if ($childPid == 0) { #  we are the child
+        # build command, command array:
+        $cmd = "COMMAND";
+        $cmdarray[] = "OPTION1";
+        $cmdarray[] = "OPTION2";
+        # etc...
 
 #    echo date('Y-m-d H:i:s ') . '[' . getmypid() . '] ' . "pcntl_exec($cmd, \$cmdarray)\n";
-    stdlog("pcntl_exec($cmd, \$cmdarray)");
-    var_dump($cmdarray);
-    pcntl_exec($cmd, $cmdarray);
-}
+        stdlog("pcntl_exec($cmd, \$cmdarray)");
+        var_dump($cmdarray);
+        pcntl_exec($cmd, $cmdarray);
+    }
 
 # --- parent process continues:
 #echo date('Y-m-d H:i:s ') . "[$myPid] waiting for child process [$childPid]\n";
-stdlog("waiting for child process [$childPid]");
-pcntl_wait($status); # Protect against Zombie children
-stdlog("child process [$childPid] returned $status");
-$childPid = 0;
+    stdlog("waiting for child process [$childPid]");
+    pcntl_wait($status); # Protect against Zombie children
+    stdlog("child process [$childPid] returned $status");
+    $childPid = 0;
+}
 
 # EOF:
