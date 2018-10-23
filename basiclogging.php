164879c951129d6fad4739ae9b6f8bcbf6e1e3d8
bill perkins <wperkins@hearst.com>
2018-10-23 08:54:17 -0400

    refactor logmsg()

--- basiclogging.php
+++ basiclogging.php
@@ -1,12 +1,10 @@
 <?php
-# --- basic logging function:
-#------------------------------------------------------------------------------
-# logmsg()
-#------------------------------------------------------------------------------
-$logfile = ""; # can be set to "", null, STDOUT, STDERR, or a filename
 
+#------------------------------------------------------------------------------
+# logmsg()- basic logging function
+#------------------------------------------------------------------------------
 function logmsg($msg) {
-    global $logfile;
+    $logfile = ""; # can be set to "", null, STDOUT, STDERR, or a filename
     if ($logfile == ""): return; endif;
     file_put_contents($logfile, date('Y-m-d H:i:s ') . "$msg\n", FILE_APPEND);
 }
